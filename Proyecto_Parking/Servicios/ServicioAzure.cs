e091f6dd4a574e83b0cdd3a28948e9dae15a8e87
Sr.J <jorge.llopis@iesdoctorbalmis.com>
2022-02-14 14:26:00 +0100

    Ahora el servicio de azure puedes subir imagenes con el mismo nombre y comentarios para la documentacion

--- Proyecto_Parking/Servicios/ServicioAzure.cs
+++ Proyecto_Parking/Servicios/ServicioAzure.cs
@@ -8,8 +8,16 @@ using Azure.Storage.Blobs;
 
 namespace Proyecto_Parking.Servicios
 {
+    /// <summary>
+    /// El servicio ServicioAzure nos permite guardar una imagen en un contenedor de Azure
+    /// </summary>
     class ServicioAzure
     {
+        /// <summary>
+        /// El metodo GuardarImagen nos permite guardar una imagen en azure
+        /// </summary>
+        /// <param name="rutaImagen">Le deberemos de pasar como parametro la ruta local de la foto que queremos guardar en Azure</param>
+        /// <returns>Nos debuelve un string el cual nos indica la ruta de la imagen en el contenedor de Azure</returns>
         public string GuardarImagen(string rutaImagen)
         {
 
@@ -17,9 +25,16 @@ namespace Proyecto_Parking.Servicios
             var clienteBlobService = new BlobServiceClient(Properties.Settings.Default.cadenaConexion);
             var clienteContenedor = clienteBlobService.GetBlobContainerClient(Properties.Settings.Default.nombreContenedorBlobs);
 
-            //Leemos la imagen y la subimos al contenedor
+            //Leemos la imagen
             Stream streamImagen = File.OpenRead(rutaImagen);
             string nombreImagen = Path.GetFileName(rutaImagen);
+
+            //Eliminamos si existe
+            BlobServiceClient blobServiceClient = new BlobServiceClient(Properties.Settings.Default.cadenaConexion);
+            BlobContainerClient cont = blobServiceClient.GetBlobContainerClient(Properties.Settings.Default.nombreContenedorBlobs);
+            cont.GetBlobClient(nombreImagen).DeleteIfExists();
+
+            //Subimos
             clienteContenedor.UploadBlob(nombreImagen, streamImagen);
 
             //Una vez subida, obtenemos la URL para referenciarla
